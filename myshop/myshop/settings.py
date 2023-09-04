722f4df1162b0b904fa9814d4b1b86c9aff83d79
VladlmlrF <vladimirfalin84@gmail.com>
2023-09-04 18:04:56 +0700

    create app cart, create class Cart

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CART_SESSION_ID = 'cart'
