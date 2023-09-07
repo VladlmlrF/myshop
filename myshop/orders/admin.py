dd61e6c07bda4a725169b444085f3a94004219ae
VladlmlrF <vladimirfalin84@gmail.com>
2023-09-07 17:25:38 +0700

    expanded the administration site with the ability to view the details of each order

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -4,6 +4,7 @@ from django.utils.safestring import mark_safe
 import csv
 import datetime
 from django.http import HttpResponse
+from django.urls import reverse
 
 
 def export_to_csv(modeladmin, request, queryset):
@@ -44,10 +45,15 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
 
 
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 'address',
-                    'postal_code', 'city', 'paid', order_stripe_payment, 'created', 'updated']
+                    'postal_code', 'city', 'paid', order_stripe_payment, 'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
